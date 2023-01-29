2056462d942dd5c7bf89af998ce9acd7a29f6d7a
Simone Teglia <simone.teglia9@gmail.com>
2023-01-29 20:59:19 +0100

    photos in sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,22 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import global from "../global.json";
 
-const clarkePhoto = "/images/clarke.jpeg";
+const clarkePhoto = "/images/clarke.jpg";
 const simoPhoto = "/images/simo.jpeg";
 const duduPhoto = "/images/dudu.jpeg";
+const gaiaPhoto = "/images/gaia.jpg";
 const jacopoPhoto = "/images/jacopo.jpeg";
+const ludonaPhoto = "/images/ludona.png";
+const claraPhoto = "/images/clara.jpeg";
+const luisaPhoto = "/images/luisa.jpg";
+const michelePhoto = "/images/michele.jpg";
+const pierPhoto = "/images/pier.jpeg";
+const sdgPhoto = "/images/sdg.jpg";
+const serenaPhoto = "/images/serena.jpg";
+const sofiaPhoto = "/images/sofia.jpg";
+const alessioPhoto = "/images/alessio.jpg";
+const mattePhoto = "/images/matte.jpg";
+const valeriaPhoto = "/images/valeria.jpg";
 
 export default function Navbar({ windowSize, nomi }) {
 	if (windowSize > global.UTILS.MOBILE_WIDTH) {
@@ -132,6 +144,28 @@ function getPhoto(nome) {
 			return duduPhoto;
 		case "Jacopo":
 			return jacopoPhoto;
+		case "Clara":
+			return claraPhoto;
+		case "Gaia":
+			return gaiaPhoto;
+		case "Ludona":
+			return ludonaPhoto;
+		case "Luisa":
+			return luisaPhoto;
+		case "Michele":
+			return michelePhoto;
+		case "Pier":
+			return pierPhoto;
+		case "SdG":
+			return sdgPhoto;
+		case "Sere":
+			return serenaPhoto;
+		case "Sofia":
+			return sofiaPhoto;
+		case "Valeria":
+			return valeriaPhoto;
+		case "Belmy":
+			return mattePhoto;
 		default:
 			return simoPhoto;
 	}
